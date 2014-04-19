478ae6a54ed7a4e6c51e7a2d378c0a4985ef8b53
jennikins813 <jennikins813@yahoo.com>
2014-04-18 22:30:15 -0400

    Creating log in functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  #dont need to instantiate a user object in new since we're only logging in, not creating a new user.
+  
+  def create
+  	user = User.find_by_email(params[:email])
+
+  	if user && user.authenticate(params[:password])
+  		session[:user_id] = user.id
+  		redirect_to products_url, :notice => "Logged in!"
+  	else
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	redirect_to products_url, :notice => "Logged out!"
+  end
+end
