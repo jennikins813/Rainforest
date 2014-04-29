8775cb0928d5deb1e2b2fed56197d1bf939ae3f5
jennikins813 <jennikins813@yahoo.com>
2014-04-29 15:32:10 -0400

    Refactor to use $.get method on AJAX search-2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,15 +20,6 @@ $(document).ready(function() {
 		event.preventDefault();
 		var searchValue = $('#search').val();
 
-		$.ajax({
-			url: '/products?search=' + searchValue,
-			type: 'GET',
-			dataType: 'html'
-		}).done(function(data){
-			console.log(data);
-			$('#products').html(data);
-		});
-
 		$.get('/products?search=' + searchValue)
 			.done(function(data) {
 				console.log(data);
